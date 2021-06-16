b5685a14c93484fb4d394063058c93a0c9174097
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-06-17 03:33:26 +0900

    add users me

--- router/middleware.go
+++ router/middleware.go
@@ -1,12 +1,37 @@
 package router
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
+	"github.com/hackathon21spring-05/linq-backend/model"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
+
+// GetMe 本番用のGetMe
+func GetMe(c echo.Context) (*model.User, error) {
+	sess, err := session.Get("sessions", c)
+	if err != nil {
+		return nil, fmt.Errorf("Failed In Getting Session:%w", err)
+	}
+	id := sess.Values["id"].(string)
+	name := sess.Values["name"].(string)
+	if len(id) == 0 || len(name) == 0 {
+		accessToken := sess.Values["accessToken"].(string)
+		if len(accessToken) == 0 {
+			return nil, errors.New("AccessToken Is Null")
+		}
+		user, err := getMe(accessToken)
+		if err != nil {
+			return nil, fmt.Errorf("Failed In Getting Me:%w", err)
+		}
+		return user, nil
+	}
+
+	return &model.User{ID: id, Name: name}, nil
+}
 
 // UserAuthMiddleware 本番用のAPIにアクセスしたユーザーを認証するミドルウェア
 func UserAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
