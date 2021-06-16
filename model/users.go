9ee8746ec8d684060df014c5305b4e2fabbe99be
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-06-16 15:55:13 +0900

    データベースの作成

--- model/users.go
+++ model/users.go
@@ -7,13 +7,13 @@ import (
 
 // User usersテーブルの構造体
 type User struct {
-	ID   string `db:"user_id" json:"user_id"`
+	ID   string `db:"id" json:"id"`
 	Name string `db:"name" json:"name"`
 }
 
 // CreateUser ユーザー作成
 func CreateUser(ctx context.Context, user *User) error {
-	_, err := db.ExecContext(ctx, "INSERT INTO users (user_id, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = name", user.ID, user.Name)
+	_, err := db.ExecContext(ctx, "INSERT INTO users (id, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = name", user.ID, user.Name)
 	if err != nil {
 		return fmt.Errorf("failed to insert user: %w", err)
 	}
