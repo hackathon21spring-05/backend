bbf607040b829e988a16ca0753881dae5cf08f1d
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-06-16 22:20:01 +0900

    データベースを修正

--- mysql/init/create_db.sql
+++ mysql/init/create_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `entrys` (
-    `id` int PRIMARY KEY NOT NULL,
+    `id` varchar(36) PRIMARY KEY NOT NULL,
     `url` text NOT NULL UNIQUE,
     `title` text NOT NULL,
     `caption` text,
@@ -17,27 +17,27 @@ CREATE TABLE IF NOT EXISTS `entrys` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `bookmarks` (
-    `id` int PRIMARY KEY NOT NULL,
+    `id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `user_id` varchar(36) NOT NULL,
-    `entry_id` int NOT NULL,
+    `entry_id` varchar(36) NOT NULL,
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY user_bookmark(`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY entry_bookmark(`entry_id`) REFERENCES `entrys`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `tags` (
-    `id` int PRIMARY KEY NOT NULL,
+    `id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `tag` varchar(32) NOT NULL,
-    `entry_id` int NOT NULL,
+    `entry_id` varchar(36) NOT NULL,
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY entry_tag(`entry_id`) REFERENCES `entrys`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `favorites` (
-    `id` int PRIMARY KEY NOT NULL,
+    `id` int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `user_id` varchar(36) NOT NULL,
-    `entry_id` int NOT NULL,
-    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `entry_id` varchar(36) NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY user_favorite(`user_id`) REFERENCES `users`(`id`),
     FOREIGN KEY entry_favorite(`entry_id`) REFERENCES `entrys`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
