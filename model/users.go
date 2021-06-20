b2dc8ddda01973d45bf82a3e5747aa4673571a1c
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-06-20 12:34:57 +0900

    /users/bookmarkのcount対応

--- model/users.go
+++ model/users.go
@@ -25,7 +25,9 @@ func CreateUser(ctx context.Context, user *User) error {
 func GetBookmarks(ctx context.Context, userId string) ([]EntryDetail, error) {
 	// 2N+1を後で解決する！！！！！！TODO
 	var entrys []Entry
-	query := "SELECT bookmarks.created_at AS created_at, entrys.id AS id, url, caption, title, thumbnail FROM bookmarks JOIN entrys ON entrys.id=bookmarks.entry_id WHERE bookmarks.user_id = ? ORDER BY bookmarks.created_at DESC"
+	query := "SELECT q.* FROM bookmarks b " +
+		"JOIN (SELECT bookmarks.created_at AS created_at, entrys.id AS id, url, caption, title, thumbnail, COUNT(bookmarks.entry_id) AS number FROM bookmarks JOIN entrys ON entrys.id=bookmarks.entry_id GROUP BY bookmarks.entry_id) q " +
+		"ON b.entry_id=q.id WHERE b.user_id = ? ORDER BY q.created_at DESC"
 	err := db.SelectContext(ctx, &entrys, query, userId)
 	if err != nil {
 		return []EntryDetail{}, fmt.Errorf("failed to get entry: %w", err)
@@ -40,15 +42,11 @@ func GetBookmarks(ctx context.Context, userId string) ([]EntryDetail, error) {
 			return []EntryDetail{}, fmt.Errorf("failed to get entry: %w", err)
 		}
 		isBookmark := true
-		entryDetails[i].ID = entry.ID
-		entryDetails[i].Url = entry.Url
-		entryDetails[i].Title = entry.Title
-		entryDetails[i].Caption = entry.Caption
-		entryDetails[i].Thumbnail = entry.Thumbnail
-		entryDetails[i].CreatedAt = entry.CreatedAt
-
-		entryDetails[i].Tags = tags
-		entryDetails[i].IsBookmark = isBookmark
+		entryDetails[i] = EntryDetail{
+			Entry:      entry,
+			Tags:       tags,
+			IsBookmark: isBookmark,
+		}
 	}
 	return entryDetails, nil
 }
